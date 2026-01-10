6a09f7f03aa4f28c7525c54feb8e693ce2aca9c5
Taz <christiantazma77@gmail.com>
2026-01-10 22:19:24 +0300

    regular commit

--- components/icons/MessageIcon.tsx
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-
-export const MessageIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193l-3.72-3.72a1.063 1.063 0 00-1.5 0l-3.72 3.72a2.123 2.123 0 01-1.98-2.193v-4.286c0-.97.616-1.813 1.5-2.097m6.72-3.72a.75.75 0 01.75.75v3.72c0 .414-.336.75-.75.75h-6.72a.75.75 0 01-.75-.75v-3.72c0-.414.336-.75.75-.75h6.72z" />
-  </svg>
-);
